38c822bb4a8298c02a4518872526716320db2cfd
drh <drh@noemail.net>
2015-09-19 14:56:29 +0000

    Remove the rebuild-json.sh script.  Adjust rebuild-main.sh to also include building in json1.c.
    FossilOrigin-Name: a010e2300a54089fc58c1d076406d31f982d9c85

--- rebuild-main.sh
+++ rebuild-main.sh
@@ -3,16 +3,16 @@
 # Run this script to rebuild the program used for fuzzing.
 #
 OBJ="fuzzershell"
-SRC="sqlite3.c fuzzershell.c"
-FILES="$SRC sqlite3.h"
+SRC="sqlite3.c fuzzershell.c json1.c"
+FILES="$SRC sqlite3.h sqlite3ext.h"
 ls -l $FILES
 sha1sum $FILES
 echo "Building from the files above...."
 OPTS="$OPTS -DSQLITE_THREADSAFE=0"
-OPTS="$OPTS -DSQLITE_ENABLE_LOAD_EXTENSION=0"
-OPTS="$OPTS -DSQLITE_NO_SYNC=0"
-OPTS="$OPTS -DSQLITE_SQLITE_DEBUG=0"
-OPTS="$OPTS -DSQLITE_OMIT_RANDOMNESS=0"
-OPTS="$OPTS -DSQLITE_ENABLE_JSON1=0"
-../afl-gcc -O3 -o $OBJ $OPTS -I. $SRC -ldl
+OPTS="$OPTS -DSQLITE_OMIT_LOAD_EXTENSION"
+OPTS="$OPTS -DSQLITE_NO_SYNC"
+OPTS="$OPTS -DSQLITE_DEBUG"
+OPTS="$OPTS -DSQLITE_OMIT_RANDOMNESS"
+OPTS="$OPTS -DSQLITE_ENABLE_JSON1"
+../afl-gcc -O3 -g -o $OBJ $OPTS -I. $SRC -ldl
 ls -l $OBJ
