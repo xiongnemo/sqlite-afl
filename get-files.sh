5d9370023023407e1bc003a04b44e715ebf2825b
drh <drh@noemail.net>
2015-09-23 00:32:38 +0000

    Add the get-files.sh convenience shell script
    FossilOrigin-Name: 49cea6fda2248d9d42884c0623d7f1c9bf48226d

---
+++ get-files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Run this script to fetch the source files need to build fuzzershell
+#
+BASE=/home/drh/sqlite
+BLD=$BASE/bld
+SRC=$BASE/sqlite
+cp $BLD/sqlite3.c $BLD/sqlite3.h $BLD/sqlite3ext.h .
+cp $SRC/tool/fuzzershell.c $SRC/ext/misc/json1.c .
